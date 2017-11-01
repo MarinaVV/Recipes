ae75dc99c7363d81f362b257b5a681a86af5faa4
mv <>
2017-11-01 15:07:52 +0200

    Create recipe fron-end

--- WebContent/WEB-INF/jsp/create-recipe.jsp
+++ WebContent/WEB-INF/jsp/create-recipe.jsp
@@ -11,6 +11,7 @@
 <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/create-recipe.css">
  
 <script type="text/javascript" src="${pageContext.request.contextPath}/js/nav-bar.js"></script>
+<script type="text/javascript" src="${pageContext.request.contextPath}/js/create-recipe.js"></script>
 </head>
 <body>
 	<div class="outer_container">
@@ -35,21 +36,44 @@
 			<div class="ingredients_images_div">
 				<div class="ingredients_div">
 					<label>Ingredients</label>
+					<input type="hidden" value="1" id="count_ingredients_input">
+					<hr>
 					<div id="ingredients">
-						<div class="ingredient">
-							<input id="ingredient_input" type="text" name="ingredients_input" value=""> <input 
-							id="quantity_input" type="text" name="quantity_input" value=""><select 
-							id="unit_select"></select><button class="delete_button" id="delete_button_1"  onclick="">-</button>
-						</div>				
+						<div class="ingredient" id="div_ingredient_1">
+							<input class="ingredient_input" id="ingredient_input_1" type="text" name="ingredients_input" 
+							value="Add ingredient" onclick="deleteHintValueIngredient(ingredient_input_1)" onblur="setHintValueIngredient(ingredient_input_1)"><input 
+							class="quantity_input" id="quantity_input_1" type="text" name="quantity_input" value="Quantity"><input 
+							class="unit_input" id="unit_input_1" value="Units" name="units_input" list="suggestions"><button 
+							class="delete_button" id="delete_button_1"  onclick="">-</button>
+						</div>		
 					</div>
+					<button id=add_button onclick="add_ingredient()">+</button>	
+					<datalist id="suggestions">
+						 <option value="Chrome">
+  						 <option value="Firefox">
+					</datalist>
 				</div>
 				<div class="images_div">
-				
+					<div class="preview_div">
+						<img id="previewHolder" alt="Uploaded Image Preview Holder" width="250px" height="250px"/>
+					</div>
+					<div class="choose_div">
+						<label>Primary image</label>
+						<input id="primary_img" type="file" accept="image/*" data-errormsg="PhotoUploadErrorMsg" onchange="readURL()">
+						<label>Image</label>
+						<input id="secondary_img_1" type="file" accept="image/*" data-errormsg="PhotoUploadErrorMsg" onchange="readURL()">
+						<label>Image</label>
+						<input id="secondary_img_2" type="file" accept="image/*" data-errormsg="PhotoUploadErrorMsg" onchange="readURL()">
+						<label>Image</label>
+						<input id="secondary_img_3" type="file" accept="image/*" data-errormsg="PhotoUploadErrorMsg" onchange="readURL()">
+						<label>Image</label>
+						<input id="secondary_img_4" type="file" accept="image/*" data-errormsg="PhotoUploadErrorMsg" onchange="readURL()">
+					</div>
 				</div>
 			</div>
 			
 			<div class="description_div">
-				<input id="description_input" type="text" value="Add Description">
+				<textarea id="description_input" cols="40" rows="5"> Add Description</textarea>
 			</div>
 			
 			<div class="last_buttons_div">
---
+++ WebContent/js/create-recipe.js
@@ -0,0 +1,83 @@
+function add_ingredient() {
+	var countIngredients = document.getElementById("count_ingredients_input").value;
+	countIngredients++;
+	var countIngredients = document.getElementById("count_ingredients_input").value = countIngredients;
+
+	var ingredientDiv = document.createElement("div");
+	ingredientDiv.setAttribute("class", "ingredient")
+	ingredientDiv.setAttribute("id", "div_ingredient_" + countIngredients);
+
+	var ingredientInput = document.createElement("input");
+	ingredientInput.setAttribute("type", "text");
+	ingredientInput.setAttribute("id", "input_ingredient_" + countIngredients);
+	ingredientInput.setAttribute("name", "ingredient_input");
+	ingredientInput.setAttribute("value", "Add ingredient");
+	ingredientInput.setAttribute("class", "ingredient_input");
+	ingredientInput.setAttribute("onclick", "deleteHintValueIngredient("
+			+ "input_ingredient_" + countIngredients + ")");
+	ingredientInput.setAttribute("onblur", "setHintValueIngredient("
+			+ "input_ingredient_" + countIngredients + ")");
+
+	var quantityInput = document.createElement("input");
+	quantityInput.setAttribute("type", "text");
+	quantityInput.setAttribute("id", "quantity_input_" + countIngredients);
+	quantityInput.setAttribute("name", "quantity_input");
+	quantityInput.setAttribute("value", "Quantity");
+	quantityInput.setAttribute("class", "quantity_input");
+
+	var unitsInput = document.createElement("input");
+	unitsInput.setAttribute("type", "text");
+	unitsInput.setAttribute("id", "unit_input_" + countIngredients);
+	unitsInput.setAttribute("name", "units_input");
+	unitsInput.setAttribute("value", "Units");
+	unitsInput.setAttribute("class", "unit_input");
+
+	var deleteButton = document.createElement("button");
+	deleteButton.setAttribute("id", "delete_button_" + countIngredients);
+	deleteButton.setAttribute("onclick", "delete_ingredient(div_ingredient_"
+			+ countIngredients + ")");
+	deleteButton.innerHTML = "-";
+	deleteButton.setAttribute("class", "delete_button");
+
+	ingredientDiv.appendChild(ingredientInput);
+	ingredientDiv.appendChild(quantityInput);
+	ingredientDiv.appendChild(unitsInput);
+	ingredientDiv.appendChild(deleteButton);
+
+	document.getElementById("ingredients").appendChild(ingredientDiv);
+}
+
+function delete_ingredient(divId) {
+
+	document.getElementById("ingredients").removeChild(divId);
+}
+
+function readURL() {
+
+	var input = document.getElementById("filechooser");
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function(e) {
+			// get loaded data and render thumbnail.
+			document.getElementById("previewHolder").src = e.target.result;
+		};
+
+		// read the image file as a data URL.
+		reader.readAsDataURL(input.files[0]);
+	} else {
+		document.getElementById("previewHolder").src = "";	
+	}
+}
+
+function deleteHintValueIngredient(element) {
+	if (element.value == "Add ingredient") {
+		element.setAttribute("value", "")
+	}
+}
+
+function setHintValueIngredient(element) {
+	if (element.value.trim() == "") {
+		element.setAttribute("value", "Add ingredient");
+	}
+}
