2e3bbaa95c2c43500e6a4ab21e3c1a6c4a12aad3
mv <>
2017-11-01 16:06:39 +0200

    Models impl

---
+++ src/web/eng/recipes/models/Image.java
@@ -0,0 +1,37 @@
+package web.eng.recipes.models;
+
+public class Image {
+
+	private long id;
+	private String imgPath;
+	private Recipe recipe;
+	//1 for primary , 0 for secondary
+	private short is_primary;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getImgPath() {
+		return imgPath;
+	}
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
+	public Recipe getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	public short getIs_primary() {
+		return is_primary;
+	}
+	public void setIs_primary(short is_primary) {
+		this.is_primary = is_primary;
+	}
+	
+	
+}
